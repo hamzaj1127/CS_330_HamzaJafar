67e8c384d5188452388c18bddd67a9d9a861f6d3
hamzaj1127 <hamzaj1127@gmail.com>
2022-12-20 09:36:23 -0500

    Add project files.

---
+++ CS_330_HamzaJafar/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+
+namespace CS_330_HamzaJafar
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+           
+         
+
+           
+            string[] question_list = System.IO.File.ReadAllLines(@"C:\Users\jafar_hamza\Downloads\quizquestions.txt");
+
+            MessageBox.Show(question_list[2]);
+
+            foreach (string line in question_list)
+            {
+                Trace.WriteLine(line);
+            }
+
+ 
+
+        }
+    }
+}
